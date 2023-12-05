469b506a68c8f36850071b5aa4d3b81a0a418d81
Artem Vetik <av.unitygames@gmail.com>
2023-12-05 17:35:06 +0300

    Create authorize function

---
+++ GameCouponsServer/Program.cs
@@ -0,0 +1,4 @@
+﻿var handler = new GameCouponsServer.Handler();
+var response = await handler.FunctionHandler(null, default);
+Console.WriteLine(response.access);
+Console.WriteLine(response.refresh);
